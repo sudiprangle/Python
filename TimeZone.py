31e54e39d0c4e41cb75f0c9240cddadabb26a4b4
Sudip Rangle <ranglesudip9@gmail.com>
2024-03-03 18:05:35 +0530

    recorrect

---
+++ TimeZone.py
@@ -0,0 +1,12 @@
+import time
+
+timestamp = int(time.strftime('%H'))
+
+if timestamp < 12:
+    print("Good Morning")
+elif 12 <= timestamp < 15:
+    print("Good Afternoon")
+elif 15 <= timestamp < 18:
+    print("Good Evening")
+else:
+    print("Good Night")
